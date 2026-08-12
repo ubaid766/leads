066e1dbbea8b3b1a619e291015e81ee4cbfe32bc
ubaid766 <ubaidullahasif18@gmail.com>
2026-08-12 21:28:18 +0500

    Update code with fixed Supabase and N8N webhook config

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://djemekbqkqclulekrgf.supabase.co'
-const supabaseAnonKey = 'sb_publishable_d49cDVZ088Z7mcsO9iMDgA_lhFBKNIJ'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZW1la2Jxa3FnY2x1bGVrcmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzODM2ODcsImV4cCI6MjEwMTk1OTY4N30.lxMLlNEDIfsSuxY3KQ2xi-sd13dl6YYWJ0qjowzhEOA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
