cad048ed1c6976413777959a09efbec6ce5831f0
ubaid766 <ubaidullahasif18@gmail.com>
2026-08-15 13:39:23 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ import {
   User as UserIcon,
 } from "lucide-react";
 
-const supabaseUrl = 'https://djemekbqkqglulekrgf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZW1la2Jxa3FnbHVsZWtyZ2YiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczOTgwMDY0NSwiZXhwIjoyMDU1Mzc2NjQ1fQ.eyJpc3MiOiJzdXBhYmFzZSIsInJvbefv';
+const supabaseUrl = 'https://djemekbqkqgclulekrgf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZW1la2Jxa3FnY2x1bGVrcmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzODM2ODcsImV4cCI6MjEwMTk1OTY4N30.lxMLlNEDIfsSuxY3KQ2xi-sd13dl6YYWJ0qjowzhEOA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
